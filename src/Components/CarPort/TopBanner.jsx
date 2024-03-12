80fa1408795efe33bab91484306f49d18b42be5e
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-03-12 22:07:21 +0600

    resolved banner

--- src/Components/CarPort/TopBanner.jsx
+++ src/Components/CarPort/TopBanner.jsx
@@ -22,7 +22,7 @@ const TopBanner = () => {
                 <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-75"></div>
 
                 {/* Navbar */}
-                <nav className="absolute top-0 left-0 right-0 flex items-center justify-between md:justify-between px-6 md:px-32 py-3 md:py-7 text-white">
+                <nav className="absolute top-0 left-0 right-0 flex items-center justify-between md:justify-between px-6 py-3 md:py-7 text-white">
                     {/* Logo */}
                     <div className="flex items-center">
                         <Link to="/"><img src="/logo_carport.png" alt="Logo" className="h-10 md:h-16" /></Link>
@@ -43,11 +43,12 @@ const TopBanner = () => {
                 {/* Foreground Text */}
                 <div className="w-full absolute top-1/2 left-1/2 md:left-32 transform -translate-x-1/2 md:-translate-x-0 -translate-y-1/2 z-10">
                     <div className="text-center md:text-start w-full">
-                        <h1 className="text-2xl md:text-5xl font-bold text-white mb-2 md:mb-4">KUKU  Maker</h1>
-                        <p className="md:text-lg text-gray-300">Take Control Of Your Coffee Taste</p>
-                        <h3 className="md:text-xl text-white md:pt-3">Pre-order now to secure a <span className="text-[#00C2FF] font-bold text-xl md:text-2xl italic">50%</span> <br /> discount on our limited early bird offer.</h3>
+                        <h1 className="text-2xl md:text-5xl font-bold text-white mb-2 md:mb-4">SHADESWIFT</h1>
+                        <p className="md:text-lg text-white">where style and protection for your vehicle come together!</p>
+                        <p className="text-sm text-gray-300 pt-3">Discover the perfect mix of sleep design and reliable protection <br /> with our shadeswift carport.</p>
+                        <h3 className="md:text-xl text-white md:pt-7">Pre-order now and grab one shadeswift carport with <span className="text-orange-600 font-bold text-xl md:text-2xl italic">48%</span> <br /> discount.</h3>
                         <Link to="/productdetails/carport/ShadeSwift">
-                            <button className="btn btn-sm md:btn-md text-xs md:text-md bg-[#00C2FF] text-white border-0 px-7 my-2 md:my-5 py-2 rounded">Pre-order Now</button>
+                            <button className="btn btn-sm text-xs md:text-md bg-orange-400 capitalize text-black hover:text-white border-0 px-7 my-2 md:my-5 py-2 rounded">Pre order on indiegogo</button>
                         </Link>
                     </div>
                 </div>
