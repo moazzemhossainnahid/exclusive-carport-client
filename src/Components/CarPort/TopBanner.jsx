8512e09b6cd49812e24514a0bc55a95ef2914553
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-03-25 12:51:10 +0600

    add meta tag

--- src/Components/CarPort/TopBanner.jsx
+++ src/Components/CarPort/TopBanner.jsx
@@ -41,14 +41,14 @@ const TopBanner = () => {
                 </nav>
 
                 {/* Foreground Text */}
-                <div className="w-full absolute top-1/3 left-1/2 md:left-20 mt-16 md:mt-20 transform -translate-x-1/2 md:-translate-x-0 -translate-y-1/2 z-10">
+                <div className="w-full absolute top-1/3 left-1/2 md:left-20 mt-16 md:mt-28 transform -translate-x-1/2 md:-translate-x-0 -translate-y-1/2 z-10">
                     <div className="text-center md:text-start w-full px-7 md:px-0">
-                        <h1 className="text-2xl md:text-5xl font-bold text-white md:mb-4">SHADESWIFT</h1>
-                        <p className="text-md md:text-lg  font-[Poppins] capitalize text-[#ECECEC]">Where Style And Protection For Your Vehicle <br /> Come Together!</p>
-                        <p className="text-md text-[#ABABAB] italic font-[Poppins]  pt-2 md:pt-3">Discover the perfect mix of sleep design and reliable <br /> protection with our shadeswift carport.</p>
+                        <h1 className="text-3xl md:text-4xl lg:text-5xl pb-3 md:pb-0 font-bold text-white md:mb-4">SHADESWIFT</h1>
+                        <p className="text-sm md:text-md md:text-lg  font-[Poppins] capitalize text-[#ECECEC]">Where Style And Protection For Your Vehicle <br /> Come Together!</p>
+                        <p className="text-sm md:text-md text-[#ABABAB] italic font-[Poppins]  pt-2 md:pt-3">Discover the perfect mix of sleep design and reliable <br /> protection with our shadeswift carport.</p>
                         <h3 className="text-sm md:text-xl text-white font-[Poppins] pt-7">Pre-order now and grab one shadeswift carport with <span className="text-[#00C2FF] font-sans font-bold text-2xl md:text-3xl italic">48%</span> <br /> discount.</h3>
                         <Link to="/productdetails/carport/ShadeSwift">
-                            <button className="btn btn-md md:btn-md text-xs md:text-md bg-[#00C2FF] dark:bg-[#00C2FF] capitalize text-black hover:text-white border-0 px-20  my-2 md:my-5 py-2 rounded">Pre-Order now on Kickstarter</button>
+                            <button className="btn btn-sm md:btn-md text-xs md:text-md bg-[#00C2FF] dark:bg-[#00C2FF] capitalize text-black hover:text-white border-0 px-5 md:px-20  my-2 md:my-5 py-2 rounded">Pre-Order now on Kickstarter</button>
                         </Link>
                     </div>
                 </div>
